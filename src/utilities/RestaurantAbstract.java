b025c35d7d6c7805b9aadd44d034dd3f6f7344b5
Dmirtiy Busygin <Busygin.voll@mail.ru>
2021-11-16 01:49:16 +0300

    Update RestaurantAbstract.java

--- src/utilities/RestaurantAbstract.java
+++ src/utilities/RestaurantAbstract.java
@@ -1,17 +1,17 @@
 package utilities;
 
-public abstract class RestaurantAbstract implements ObjectInterface{
+public abstract class RestaurantAbstract implements BuildingInterface {
     protected boolean common;
 
     public abstract void hasTerrace();
 
     public abstract void hasOutsideService();
 
-    public boolean isCommon(){
+    public boolean isCommon() {
         return common;
     }
 
-    public void setCommon(boolean common){
+    public void setCommon(boolean common) {
         this.common = common;
     }
 }
