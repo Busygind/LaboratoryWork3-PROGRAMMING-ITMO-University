1703c7818d0b34e10e90cbde58bf902e31f8d76f
Dmirtiy Busygin <Busygin.voll@mail.ru>
2021-11-17 21:48:05 +0300

    Update RestaurantAbstract.java

--- src/utilities/RestaurantAbstract.java
+++ src/utilities/RestaurantAbstract.java
@@ -5,13 +5,15 @@ import entities.Person;
 public abstract class RestaurantAbstract implements BuildingInterface {
     protected boolean isCommon;
 
-    public abstract void hasTerrace();
+    public abstract boolean hasTerrace();
 
-    public abstract void hasOutsideService(Person waiter);
+    public abstract boolean hasOutsideService();
 
     public boolean restaurantIsCommon() {
         return isCommon;
     }
+
+    public abstract void getOutsideServiceAvialability(Person waiter);
 
     public void setCommon(boolean common) {
         this.isCommon = common;
