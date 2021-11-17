37548e9b4c7e6ce8ab4f7671ea3d47c421c56e97
Dmirtiy Busygin <Busygin.voll@mail.ru>
2021-11-17 21:58:58 +0300

    Create Person.java

---
+++ src/utilities/Person.java
@@ -0,0 +1,9 @@
+package utilities;
+
+import entities.Infrastructure;
+
+public abstract class Person implements ObjectInterface {
+    public abstract void walkBy(Infrastructure infrastructure);
+
+    public abstract void jumpOut();
+}
